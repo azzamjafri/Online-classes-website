6999d9ba11cab19a71c23ee9a26ba0c84c57ca52
azzamjafri <azzamjafri98@gmail.com>
2020-04-23 16:45:55 +0530

    Initial commit

---
+++ src/main/java/trs/jmi/trsjmi/controller/HomeController.java
@@ -0,0 +1,25 @@
+package trs.jmi.trsjmi.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+@RequestMapping("")
+public class HomeController {
+
+//    @RequestMapping(value = "/members", method = RequestMethod.GET)
+//    public String home(){
+//        return "home/members";
+//    }
+//
+//
+//    @RequestMapping(value = "/events", method = RequestMethod.GET)
+//    public String events(){
+//        return "TRSEvents/events";
+//    }
+
+
+}
